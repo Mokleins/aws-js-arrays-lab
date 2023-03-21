048e57f1cbe952eea9a77aeb295d01e74736ec86
Xylon <xyfullns@gmail.com>
2023-03-21 18:00:19 -0400

    solution

--- index.js
+++ index.js
@@ -3,3 +3,42 @@ let cats = ["Milo", "Otis", "Garfield"]
 
 // Write your solutions here!
 
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const copyCats = [...cats];
+    copyCats.push(name);
+    return copyCats;
+}
+
+function prependCat(name){
+    const copyCats = [...cats];
+    copyCats.unshift(name);
+    return copyCats;
+}
+
+function removeLastCat(){
+    const copyCats = [...cats];
+    copyCats.pop();
+    return copyCats;
+}
+
+function removeFirstCat(){
+    const copyCats = [...cats];
+    copyCats.shift();
+    return copyCats;
+}
